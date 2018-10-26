fc9b095a9b969c6a0bc3379dff05250a6317cacf
Imran Khan <imran.khan@kpn.com>
2018-10-26 16:15:46 +0200

    now includes a Note counter

--- app/main.js
+++ app/main.js
@@ -1,3 +1,5 @@
+const EventBus = new Vue();
+
 const inputComponent = {
   template: `<input 
                 class="input is-small"
@@ -13,7 +15,7 @@ const inputComponent = {
     },
     methods: {
         monitorEnterKey() {
-            this.$emit('add-note', {
+            EventBus.$emit('add-note', {
                 note: this.input,
                 timestamp: new Date().toLocaleString()
             });
@@ -22,11 +24,26 @@ const inputComponent = {
     }
 };
 
+const noteCountComponent = {
+    template: `<div class="note-count">
+                Note count: <strong> {{ noteCount }}</strong>
+                </div>`,
+    data () {
+        return {
+            noteCount: 0
+        }
+    },
+    created() {
+        EventBus.$on('add-note', event => this.noteCount++);
+    }
+};
+
 // Root instance
 new Vue({
   el: '#app',
   components: {
-    'input-component': inputComponent
+    'input-component': inputComponent,
+    'note-count-component': noteCountComponent
   },
     data: {
         notes: [],
@@ -38,5 +55,8 @@ new Vue({
           this.notes.push(event.note);
           this.timestamps.push(event.timestamp);
       }
+    },
+    created() {
+      EventBus.$on('add-note', event => this.addNote(event));
     }
 });
